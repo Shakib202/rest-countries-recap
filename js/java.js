a902732f7c46d5e45b5a723ea5b57c2fd50e8ade
Shakib202 <shakib2020360@gmail.com>
2022-03-16 01:53:31 +0600

    last commit

--- js/java.js
+++ js/java.js
@@ -11,13 +11,35 @@ const displayCountries = countries => {
   const container = document.getElementById('countries');
   container.innerHTML = countriesHTML.join(' ');
 }
-const getCountryHTML = country => {
-  const {name, flags} = country;
+// option 2
+const getCountryHTML = ({name, flags, area, region}) => {
   return `
     <div class="country">
        <h2>${name.common}</h2>
+       <p>Area: ${area}</p>
+       <p>Continet: ${region}</p>
        <img src="${flags.png}">
     </div>
   `
 }
+// option 1
+// const getCountryHTML = country => {
+//   const {name, flags} = country;
+//   return `
+//     <div class="country">
+//        <h2>${name.common}</h2>
+//        <img src="${flags.png}">
+//     </div>
+//   `
+// }
+
+// original 
+// const getCountryHTML = country => {
+//   return `
+//     <div class="country">
+//        <h2>${country.name.common}</h2>
+//        <img src="${country.flags.png}">
+//     </div>
+//   `
+// }
 loadCountries()
